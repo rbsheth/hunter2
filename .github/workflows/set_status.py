700333b09de98479a5e6f07eb50d68d9f4f561b1
Rahul Sheth <rahul@snap.com>
2020-09-23 21:17:06 -0400

    Try new workflow

--- .github/workflows/set_status.py
+++ .github/workflows/set_status.py
@@ -1,5 +1,7 @@
 import sys
 import json
+import os
+import subprocess
 
 try:
   with open('job_data.json') as json_data:
@@ -7,12 +9,16 @@ try:
 except IOError:
   sys.exit('Can\'t read job status from job_data.json')
 
-projects = dict()
+blacklisted_toolchains = []
 
 for job in job_data['jobs'][1:-1]:
-  project = job['name'].split(" ")[0]
-  projects.setdefault(project, []).append(job)
+  toolchain = job['name'].split(" ")[1]
+  if job['conclusion'] == "failure":
+    blacklisted_toolchains.append(toolchain)
 
-for project, jobs in projects.items():
-  with open('job_data/' + project + '.json', 'w') as file:
-    json.dump(sorted(jobs, key = lambda i: i['name']), file, indent=4)
+project_dir = 'cmake/projects/' + project
+os.makedirs(project_dir + "/ci", exist_ok=True)
+
+blacklist_path = project_dir + '/ci/blacklist.json'
+with open(blacklist_path, 'w') as file:
+  json.dumps({"blacklist":blacklisted_toolchains}, file, indent=4, sort_keys=True)
